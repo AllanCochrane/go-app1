352d268f52bd55b2ede4ebe5fbd749046b8f5b95
Allan Cochrane <Allan.Cochrane@gmail.com>
2014-04-04 21:07:19 -0500

    First file

---
+++ f1.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	// "flag"
+
+	fmt "fmt"
+	"github.com/go-martini/martini"
+) // Package implementing formatted I/O.
+
+func main() {
+	fmt.Printf("Hello, world;\n")
+	fmt.Printf("Something else %d\n", 5)
+}
